287b582a0aa186a86bd8216abcf2ce22d26c1df0
Scott Lahteine <sourcetree@thinkyhead.com>
2017-01-25 07:34:53 -0800

    Fix build script paths, use bash, run in emulator

--- bB/2600basic.sh
+++ bB/2600basic.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 echo "Starting build of $1"
-preprocess<$1 | 2600basic -i $bB>bB.asm
+
+#z26="/Path/To/z26"
+#Stella="/Path/To/Stella"
+
+[ -f ~/.profile ] && source ~/.profile
+
+preprocess < "$1" | 2600basic -i "$bB" > bB.asm
 if [ "$?" -ne "0" ]
  then
   echo "Compilation failed."
@@ -8,19 +14,25 @@ if [ "$?" -ne "0" ]
 fi
 if [ "$2" = "-O" ]
   then
-   postprocess -i $bB|optimize>$1.asm
+   postprocess -i "$bB" | optimize > "$1.asm"
   else
-   postprocess -i $bB>$1.asm
+   postprocess -i "$bB" > "$1.asm"
 fi
-dasm $1.asm -I$bB/includes -f3 -o$1.bin | sed '/Label mismatch/d' \
+dasm "$1.asm" -I"$bB/includes" -f3 -o"$1.bin" | sed '/Label mismatch/d' \
 | sed '/shakescreen/d;/rand16/d;/debugscore/d;/pfscore/d;/noscore/d;/vblank_bB_code/d;/PFcolorandheight/d;/pfrowheight/d;/pfres/d;/PFmaskvalue/d;/overscan_time/d;/vblank_time/d;/no_blank_lines/d;/superchip/d;/ROM2k/d;/NO_ILLEGAL_OPCODES/d;/minikernel/d;/debugcycles/d;/mincycles/d;/legacy/d;/PFcolors/d;/playercolors/d;/player1colors/d;/backgroundchange/d;/readpaddle/d;/multisprite/d;/PFheights/d;/bankswitch/d;/Unresolved Symbols/d' \
-| sed '2,/-->/!{ /-->/,/-->/d; }'\
-| sed 's/--> 0./Possible duplicate label: /' 
+| sed '2,/-->/!{ /-->/,/-->/d; }' \
+| sed 's/--> 0./Possible duplicate label: /'
+
 if [ "$?" -ne "0" ]
  then
   echo "Assembly failed."
   exit
 fi
+
 echo "Build complete."
+
+[ -f "$z26" ] && "$z26" "$1.bin"
+[ -f "$Stella" ] && "$Stella" "$1.bin"
+
 exit
 
